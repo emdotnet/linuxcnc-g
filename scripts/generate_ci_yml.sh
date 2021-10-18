eab0495571a6f0ee8bafe36e3a496965014c5dc1
Tim Blume <tb@ciphron.de>
2021-10-18 18:29:42 +0200

    try caching

--- scripts/generate_ci_yml.sh
+++ scripts/generate_ci_yml.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ls ../tests/ | xargs -I FOO sh -c 'sed -r "s/NAME/FOO/g" github-test-template.yml' >> ../.github/workflows/ci.yml
+find ../tests/ -maxdepth 1 -type d | cut -d "/" -f 3 | xargs -I FOO sh -c 'sed -r "s/NAME/FOO/g" github-test-template.yml' >> ../.github/workflows/ci.yml
